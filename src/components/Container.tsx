ec3732986fd6fdb09e6be1aa132a474b2a0a951a
Gabriel Dias <g.dias1@hotmail.com>
2022-02-24 07:11:48 -0300

    Testes para os componentes do banner principal

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Container = ({ children, className }: Props) => {
+  return (
+    <div className={`${className} container mx-auto max-w-6xl`}>{children}</div>
+  );
+};
+
+export default Container;
